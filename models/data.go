d4d1993a088c4753a4d1ba19304b0c0b8d2f90f1
Yevhen <iorhachov.yevhen@482.solutions>
2020-01-18 13:06:34 +0200

    change struct. Add new unit tests. Change old

--- models/data.go
+++ models/data.go
@@ -3,10 +3,18 @@ package models
 import (
 	"crypto/sha256"
 	"encoding"
+	"encoding/hex"
 	"encoding/json"
+	"log"
 )
 
 type Hash32 [32]byte
+
+func (h Hash32) String() string {
+	bytes := h[:]
+
+	return hex.EncodeToString(bytes)
+}
 
 type Data interface {
 	encoding.BinaryMarshaler
@@ -14,30 +22,96 @@ type Data interface {
 	Hash() Hash32
 }
 
+type Content []byte
+
+type MetaData struct {
+	Title  string `json:"title"`
+	Format string `json:"format"`
+}
+
 type SimpleData struct {
-	Content []byte `json:"content"`
+	MetaData `json:"meta_data"`
+	Content  `json:"content"`
 }
 
-func (s *SimpleData) Hash() Hash32{
-	return sha256.Sum256(s.Content)
+func (s *SimpleData) MarshalBinary() (data []byte, err error) {
+	return json.Marshal(*s)
 }
 
-func (s *SimpleData) MarshalBinary() (data []byte, err error){
-	return json.Marshal(s)
-}
-
-func (s *SimpleData) UnmarshalBinary(data []byte) error  {
+func (s *SimpleData) UnmarshalBinary(data []byte) error {
 	return json.Unmarshal(data, s)
 }
 
+func (s *SimpleData) Hash() Hash32 {
+	data, err := s.MarshalBinary()
+	if err != nil {
+		log.Println(err)
+	}
+
+	return sha256.Sum256(data)
+}
+
 type JsonData struct {
-	SimpleData
+	Content
+}
+
+func (jsonData *JsonData) MarshalBinary() (data []byte, err error) {
+	return json.Marshal(*jsonData)
+}
+
+func (jsonData *JsonData) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, jsonData)
+}
+
+func (jsonData *JsonData) Hash() Hash32 {
+	data, err := jsonData.MarshalBinary()
+	if err != nil {
+		log.Println(err)
+	}
+
+	return sha256.Sum256(data)
 }
 
 type AudioData struct {
-	SimpleData
+	MetaData `json:"meta_data"`
+	Content  `json:"content"`
+}
+
+func (ad *AudioData) MarshalBinary() (data []byte, err error) {
+	return json.Marshal(*ad)
+}
+
+func (ad *AudioData) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, ad)
+}
+
+func (ad *AudioData) Hash() Hash32 {
+	data, err := ad.MarshalBinary()
+	if err != nil {
+		log.Println(err)
+	}
+
+	return sha256.Sum256(data)
 }
 
 type VideoData struct {
-	Frames []SimpleData
+	MetaData `json:"meta_data"`
+	Frames   []Content `json:"frames"`
+}
+
+func (vd *VideoData) MarshalBinary() (data []byte, err error) {
+	return json.Marshal(*vd)
+}
+
+func (vd *VideoData) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, vd)
+}
+
+func (vd *VideoData) Hash() Hash32 {
+	data, err := vd.MarshalBinary()
+	if err != nil {
+		log.Println(err)
+	}
+
+	return sha256.Sum256(data)
 }
