9f763596fc42420a92182c49f5b974bacdae22c8
Yevhen <iorhachov.yevhen@482.solutions>
2020-01-23 12:57:21 +0200

    add composeKey. change dataPrefix

--- storage/storage.go
+++ storage/storage.go
@@ -16,11 +16,11 @@ const (
 	PrefixVideo  Prefix = "video"
 )
 
-var DataTypeName = map[Prefix]models.DataType{
-	PrefixSimple: models.Simple,
-	PrefixJSON:   models.JSON,
-	PrefixAudio:  models.Audio,
-	PrefixVideo:  models.Video,
+var DataPrefix = map[models.DataType]Prefix{
+	models.Simple: PrefixSimple,
+	models.JSON:   PrefixJSON,
+	models.Audio:  PrefixAudio,
+	models.Video:  PrefixVideo,
 }
 
 type DataKeeper interface {
@@ -36,8 +36,8 @@ type Storage struct {
 	options badger.Options
 }
 
-func NewStorageWithOptions() *badger.Options {
-	return &badger.Options{}
+func NewStorageWithOptions() *Storage {
+	return &Storage{}
 }
 
 func NewDefaultStorage() *Storage {
@@ -78,9 +78,9 @@ func (s *Storage) Add(data models.Data) error {
 	return err
 }
 
-func (s *Storage) Read(key models.Hash32, data models.Data) error {
+func (s *Storage) Read(hash models.Hash32, data models.Data) error {
 	err := s.db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get(key[:])
+		item, err := txn.Get(hash[:])
 		if err != nil {
 			return err
 		}
@@ -99,9 +99,9 @@ func (s *Storage) Read(key models.Hash32, data models.Data) error {
 	return nil
 }
 
-func (s *Storage) Delete(key models.Hash32) error {
+func (s *Storage) Delete(hash models.Hash32) error {
 	err := s.db.Update(func(txn *badger.Txn) error {
-		return txn.Delete(key[:])
+		return txn.Delete(hash[:])
 	})
 	if err != nil {
 		return errors.New("deleting finished with error: " + err.Error())
@@ -112,4 +112,13 @@ func (s *Storage) Delete(key models.Hash32) error {
 
 func (s *Storage) Close() error {
 	return s.db.Close()
+}
+
+func ComposeKey(hash32 models.Hash32, dataType models.DataType) (key []byte) {
+	prefix := []byte(DataPrefix[dataType])
+
+	key = append(key, prefix...)
+	key = append(key, hash32[:]...)
+
+	return key
 }
